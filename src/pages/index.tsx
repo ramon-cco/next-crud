9c6ad59518eb58e0394c28d2ca0781cc853b44b1
Ramon Oliveira <77456126+ramon-cco@users.noreply.github.com>
2022-09-07 01:03:45 -0300

    create components:  title and layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../components/Layout'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={`
     flex h-screen justify-center items-center
-    bg-gradient-to-r from-purple-500 via-yellow-500 to-blue-600
+    bg-gradient-to-r from-purple-500 to-blue-600
+    text-white
     `
     }>
-      <span className='text-4xl'>Texto</span>
+        <Layout title='Cadastro simples'>
+          <span>Conteudo</span>
+        </Layout> 
     </div>
   )
 }
